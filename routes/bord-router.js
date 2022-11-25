3c053a43f22b861a7d164636f813abb87c25d7c8
mookseong <kmkook12312@gmail.com>
2022-11-25 09:52:59 +0900

    feat : router 추가 및 service 정의

---
+++ routes/bord-router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const router = express.Router();
+router.get((req, res, next) =>{
+
+});
+
+
+router.use((req, res, next) => {
+    next('Not found error!');
+});
+
+
+router.use((err, req, res, next) => {
+    res.status(500).send(err);
+});
+
+module.exports = router;
---
+++ routes/file-management-router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const router = express.Router();
+
+//upload
+
+//list
+
+
+//donwload
+
+
+
+
+
+router.use((req, res, next) => {
+    next('Not found error!');
+});
+
+
+router.use((err, req, res, next) => {
+    res.status(500).send(err);
+});
+
+module.exports = router;
