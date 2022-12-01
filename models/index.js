7993ceaab0dc50d74164e7525d15324cf01f42e4
Donghwi Kim <ehdgnl0904@naver.com>
2022-12-01 09:33:25 +0900

    refactor: Bord 관련 코드 추가

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const User = require('./user');
 const Comment = require('./comment');
+const Bord = require('./bord');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -15,11 +16,15 @@ db.Sequelize = Sequelize;
 
 db.User = User;
 db.Comment = Comment;
+db.Bord = Bord
 
 User.init(sequelize);
 Comment.init(sequelize);
+Bord.init(sequelize);
+
 
 User.associate(db);
 Comment.associate(db);
+Bord.associate(db);
 
 module.exports = db;
