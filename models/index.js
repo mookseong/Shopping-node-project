4989850b2a2898e0c89899f752d7ee4a8b5f4940
mookseong <kmkook12312@gmail.com>
2022-12-06 21:12:56 +0900

    refactor : index code refactoring

--- models/index.js
+++ models/index.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize');
 const User = require('./user');
 const Comment = require('./comment');
 const Board = require('./board');
-const File = require('./file');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -18,16 +17,13 @@ db.Sequelize = Sequelize;
 db.User = User;
 db.Comment = Comment;
 db.Board = Board;
-db.File = File
 
 User.init(sequelize);
 Comment.init(sequelize);
 Board.init(sequelize);
-File.init(sequelize);
 
 User.associate(db);
 Comment.associate(db);
 Board.associate(db);
-File.associate(db);
 
 module.exports = db;
